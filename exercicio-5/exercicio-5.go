4c2b2c1ee97325dfbf06a494a0fe8b69925a1158
Thirki <henriquegomesdev@outlook.com>
2022-08-18 13:39:48 -0300

    feat: exercise 5

---
+++ exercicio-5/exercicio-5.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Ix int
+
+var x Ix
+var y int
+
+func main() {
+	fmt.Printf("Valor: %v, Tipo: %T\n", x, x)
+	x = 42
+	fmt.Println("Novo Valor:", x)
+	y = int(x)
+	fmt.Printf("Valor: %v, Tipo: %T\n", y, y)
+}
+
+// - Utilizando a solução do exercício anterior:
+//     1. Em package-level scope, utilizando a palavra-chave var, crie uma variável com o identificador "y".
+//     O tipo desta variável deve ser o tipo subjacente do tipo que você criou no exercício anterior.
+//     2. Na função main:
+//         1. Isto já deve estar feito:
+//             1. Demonstre o valor da variável "x"
+//             2. Demonstre o tipo da variável "x"
+//             3. Atribua 42 à variável "x" utilizando o operador "="
+//             4. Demonstre o valor da variável "x"
+//         2. Agora faça tambem:
+//             1. Utilize conversão para transformar o tipo do valor da variável "x" em seu tipo subjacente e,
+//             utilizando o operador "=", atribua o valor de "x" a "y"
+//             2. Demonstre o valor de "y"
+//             3. Demonstre o tipo de "y"
