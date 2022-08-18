21dadd1ffc584b654542bc21c537664acb8daba6
CiceroSantosDB1 <cicero.santos@db1.com.br>
2022-08-18 10:51:25 -0300

    fix: exercise-1

---
+++ exercicio-2/exercicio-2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Printf("Tipo: %T\nValor: %v\n", x, x)
+	fmt.Printf("Tipo: %T\nValor: %q\n", y, y)
+	fmt.Printf("Tipo: %T\nValor: %v\n", z, z)
+	fmt.Println("\nPergunta: O compilador atribuiu valores para essas variáveis. Como esses valores se chamam?")
+	fmt.Println("Resposta: Valor 0")
+}
+
+// - Use var para declarar três variáveis. Elas devem ter package-level scope. Não atribua valores a estas variáveis.
+// Utilize os seguintes identificadores e tipos para estas variáveis:
+//     1. Identificador "x" deverá ter tipo int
+//     2. Identificador "y" deverá ter tipo string
+//     3. Identificador "z" deverá ter tipo bool
+// - Na função main:
+//     1. Demonstre os valores de cada identificador
+//     2. O compilador atribuiu valores para essas variáveis. Como esses valores se chamam?
