c5071df17e61913d3585b019893dd222153a97c4
Thirki <henriquegomesdev@outlook.com>
2022-08-19 09:11:08 -0300

    feat: nivel-2/exercicio-1

---
+++ Nivel-2/exercicio-1/exercicio-1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 100
+	fmt.Printf("Decimal: %d\t Binário: %b\t Hexadecimal: %#x", x, x, x)
+}
+
+// Escreva um programa que mostre um número em decimal, binário, e hexadecimal.
