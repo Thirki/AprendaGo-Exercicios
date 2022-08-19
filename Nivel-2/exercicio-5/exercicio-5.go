c18c74825da9447664a2e4752fd1966aca94745f
Thirki <henriquegomesdev@outlook.com>
2022-08-19 11:30:17 -0300

    feat: nivel-2/exercicio-5

---
+++ Nivel-2/exercicio-5/exercicio-5.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var x string = `Hello`
+
+func main() {
+	fmt.Printf("Valor: %v\tTipo: %T", x, x)
+}
+
+// @TODO:
+// - Crie uma variável de tipo string utilizando uma raw string literal.
+// - Demonstre-a.
