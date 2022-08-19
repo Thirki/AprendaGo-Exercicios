eb6c58d06fbfd8e21383cd3a60e476a779243812
Thirki <henriquegomesdev@outlook.com>
2022-08-19 10:57:09 -0300

    feat: nivel-2/exercicio-3

---
+++ Nivel-2/exercicio-3/exercicio-3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const a int = 10
+const b = 20
+
+func main() {
+	fmt.Printf("Const 1:\n Valor: %v\tTipo: %T", a, a)
+	fmt.Printf("\nConst 2:\n Valor: %v\tTipo: %T", b, b)
+}
+
+// - Crie constantes tipadas e não-tipadas.
+// 	- Demonstre seus valores.
