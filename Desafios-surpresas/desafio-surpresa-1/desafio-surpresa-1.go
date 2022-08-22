c61661c302e5a23ca7cc2e784f4db13227081d4e
Thirki <henriquegomesdev@outlook.com>
2022-08-22 09:03:15 -0300

    feat: Desafios-surpresas/desafio-surpresa-1

---
+++ Desafios-surpresas/desafio-surpresa-1/desafio-surpresa-1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 33; i <= 122; i++ {
+		fmt.Printf("%v - %c\n", i, i)
+	}
+}
+
+// @TODO:
+// Desafio surpresa!
+// - Faça um loop dos números 33 a 122, e utilize format printing para demonstrá-los como texto/string.
