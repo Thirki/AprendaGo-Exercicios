75d947f36591f35f6536f7b26b3a4353c8d763ba
Thirki <henriquegomesdev@outlook.com>
2022-08-22 14:38:00 -0300

    feat: Nivel-3/exercicio-6

---
+++ Nivel-3/exercicio-6/exercicio-6.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	numero := 10
+	if numero <= 20 {
+		fmt.Println("Numero menor que 20!")
+	}
+}
+
+// @TODO:
+// - Crie um programa que demonstre o funcionamento da declaração if.
