ec26b8d5c2729a65965cd2b2b5c75d235c1f9ed4
Thirki <henriquegomesdev@outlook.com>
2022-08-22 11:32:53 -0300

    feat: Nivel-3/exercicio-1

---
+++ Nivel-3/exercicio-1/exercicio-1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	for numero := 0; numero <= 10000; numero++ {
+		fmt.Print(numero, " ")
+	}
+}
+
+// @TODO:
+// Põe na tela: todos os números de 1 a 10000.
