d8f2f3c057f74be743d34344b7737c1c6bb91cad
Thirki <henriquegomesdev@outlook.com>
2022-08-29 13:34:55 -0300

    feat: Nivel-7/exercicio-1

---
+++ Nivel-7/exercicio-1/exercicio-1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	fmt.Println(&x)
+}
+
+// @TODO:
+// - Crie um valor e atribua-o a uma variável.
+// - Demonstre o endereço deste valor na memória.
