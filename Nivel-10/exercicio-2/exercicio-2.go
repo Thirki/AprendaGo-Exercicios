ef17909be39b2f0fb72a04892b3ab549ee7125df
Thirki <henriquegomesdev@outlook.com>
2022-09-13 14:48:18 -0300

    feat: Nivel-10/exercicio-2

---
+++ Nivel-10/exercicio-2/exercicio-2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
+}
+
+// @TODO:
+// - Faça esse código funcionar: https://play.golang.org/p/oB-p3KMiH6
